14a0ecced975ecca292072e283e7a27b17a35bda
Jonathan Moore <jtm170300@utdallas.edu>
2020-03-17 16:02:17 -0500

    Semantic refactor bindings

--- init.el
+++ init.el
@@ -141,6 +141,21 @@ T: 0-100"
 (global-set-key (kbd "C-c l") #'eglot-format)
 
 
+
+;; Semantic refactor bindings
+(require 'srefactor)
+(require 'srefactor-lisp)
+
+;; OPTIONAL: ADD IT ONLY IF YOU USE C/C++. 
+(semantic-mode 1) ;; -> this is optional for Lisp
+
+(define-key c-mode-map (kbd "M-RET") 'srefactor-refactor-at-point)
+(define-key c++-mode-map (kbd "M-RET") 'srefactor-refactor-at-point)
+(global-set-key (kbd "M-RET o") 'srefactor-lisp-one-line)
+(global-set-key (kbd "M-RET m") 'srefactor-lisp-format-sexp)
+(global-set-key (kbd "M-RET d") 'srefactor-lisp-format-defun)
+(global-set-key (kbd "M-RET b") 'srefactor-lisp-format-buffer)
+
 ;; Charles's custom functions
 
 (defun save-all ()
